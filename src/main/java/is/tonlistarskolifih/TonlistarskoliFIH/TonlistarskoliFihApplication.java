0562982a1e7f96ec2d3aac1fa141a3da5a2ddc82
arnihl <arnihl@gmail.com>
2019-10-30 19:20:41 +0000

    init commit

---
+++ src/main/java/is/tonlistarskolifih/TonlistarskoliFIH/TonlistarskoliFihApplication.java
@@ -0,0 +1,13 @@
+package is.tonlistarskolifih.TonlistarskoliFIH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonlistarskoliFihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonlistarskoliFihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/tonlistarskolifih/TonlistarskoliFIH/TonlistarskoliFihApplicationTests.java
@@ -0,0 +1,13 @@
+package is.tonlistarskolifih.TonlistarskoliFIH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonlistarskoliFihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
